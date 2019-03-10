c89ab3233874cef48da88791e359d3e2b4cdfb9d
Nelmeris <KufaevArtem@icloud.com>
2019-03-10 18:32:33 +0300

    Initial Commit

---
+++ Ping-Pong-Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ping-Pong-Game
+//
+//  Created by Artem Kufaev on 10/03/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ping-Pong-Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ping-Pong-Game
+//
+//  Created by Artem Kufaev on 10/03/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
